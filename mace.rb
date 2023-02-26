1906d149a55b4e17c95549df9eb7c60ea3dccfc8
jim <jim@gmail.com>
2023-02-27 03:43:22 +0800

    update

--- mace.rb
+++ mace.rb
@@ -4,8 +4,8 @@
 class Mace < Formula
   desc ""
   homepage "https://github.com/zhanqi/mace"
-  url "https://github.com/jim928/mace/archive/refs/tags/v0.1.8.tar.gz"
-  sha256 "d448f2cbf5f9e5f9db111826a9b337b25b478b710c7be6e38f4aa0d35e66e5e6"
+  url "https://github.com/jim928/mace/archive/refs/tags/v0.1.9.tar.gz"
+  sha256 "bef5e6c4c468471c24373b15e9ec6e7a7035f28fdcb4e99c3f32bcdbca0c0fb8"
   license "MIT"
 
   # depends_on "cmake" => :build
