6b82b19af2bc4fe8dda1b93463985448c4bb0255
griffin <griffin@gmail.com>
2023-12-14 11:39:09 +0800

    v0.1.13

--- mace.rb
+++ mace.rb
@@ -4,8 +4,8 @@
 class Mace < Formula
   desc ""
   homepage "https://github.com/zhanqi/mace"
-  url "https://github.com/jim928/mace/archive/refs/tags/v0.1.12.tar.gz"
-  sha256 "4031756f8fb26ed3c0e34a47524d5eccb3cbee406b69762a1eab56d64e02d663"
+  url "https://github.com/jim928/mace/archive/refs/tags/v0.1.13.tar.gz"
+  sha256 "2f9a87f833ae1672bd3912725ab5ec4e88a18d6c0c56cf1f17340114f90bd721"
   license "MIT"
 
   # depends_on "cmake" => :build
