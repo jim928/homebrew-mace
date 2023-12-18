5f108395946fcdf4b70bb6b43eb8b44b236974b6
griffin <griffin@gmail.com>
2023-12-19 04:52:16 +0800

    publish mace.rb commit from mace}.

--- mace.rb
+++ mace.rb
@@ -1,17 +1,12 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Mace < Formula
-  desc ""
-  homepage "https://github.com/zhanqi/mace"
-  url "https://github.com/jim928/mace/archive/refs/tags/v0.1.14.tar.gz"
-  sha256 "4c0091af6a14146afefa3a04db7e7048b74dd76aeba4205e186ecdb915fb8431"
-  license "MIT"
+class mace < Formula
+  desc "Your description here"
+  homepage "https://github.com/jim928/mace"
+  url "https://github.com/jim928/mace/archive/refs/tags/0.1.44.tar.gz"
+  sha256 "e8f70b592a053e83fa6a12b0184ef79398cc0b35f54a2dd7f84100276b4317d0"
 
-  # depends_on "cmake" => :build
+  # ... 其他 Formula 配置信息 ...
 
   def install
     bin.install 'mace'
   end
-
 end
