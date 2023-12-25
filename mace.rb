bca26ca200cfa51eb4a27e51a6f3eefa2a405b22
griffin <griffin@gmail.com>
2023-12-25 23:32:45 +0800

    publish mace.rb 0.1.74 commit from mace}.

--- mace.rb
+++ mace.rb
@@ -1,8 +1,8 @@
 class Mace < Formula
   desc "Your description here"
   homepage "https://github.com/jim928/mace"
-  url "https://github.com/jim928/mace/archive/refs/tags/0.1.73.tar.gz"
-  sha256 "90052a4b367bc97c6fbeab6a6a622b1106e51bd32e9b30c49447b650bc615245"
+  url "https://github.com/jim928/mace/archive/refs/tags/0.1.74.tar.gz"
+  sha256 "679e0181e0e9680492b2e6a719d3a3543c1f8cc73d905b143bd0dd786feb3e2c"
 
   # ... 其他 Formula 配置信息 ...
 
